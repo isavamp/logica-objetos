7d843ea04f22cbf56a7b345929dbc2e97655a9af
isavamp <isadora.7791@colegioprogresso.g12.br>
2026-08-13 07:37:12 -0300

    fiz os outros big sevennnnnn

--- app.js
+++ app.js
@@ -17,15 +17,21 @@ causaDano(personagem){
     }
 }
 }
-const druida = new Personagem("Obin", 150, 15)
+const druida = new Personagem("Obin", 100, 15)
 const guerreiro = new Personagem("Gaia", 60, 20)
 const mago = new Personagem("Feiticeira", 30, 35)
 const arqueiro = new Personagem("Akaith", 80, 25)
+const elfo = new Personagem("Drazio", 35, 15)
+const dragao = new Personagem("Shibuya", 200, 80)
+const mutante = new Personagem("tempestade", 70, 50)
+
 
 console.log(druida)
 console.log(guerreiro)
 console.log(mago)
 console.log(arqueiro)
+console.log(elfo)
+console.log(dragao)
+console.log(mutante)
 
-
-    
+dragao.causaDano(mago)
