ee16033ffe26dc99348a22e1cc0208f33d3cf9ce
isavamp <isadora.7791@colegioprogresso.g12.br>
2026-08-31 09:13:28 -0300

    fizemos a defesa

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 class Personagem{
-constructor(name,life,atack){
+constructor(name,life,atack,defesa){
     this.name = name
     this.life = life
     this.atack = atack
+    this.defesa = defesa
 }
 
 causaDano(personagem){
@@ -10,18 +11,23 @@ causaDano(personagem){
 
 }
  recebeDano(quantidade){
-    this.life = this.life - quantidade
+    let dano = quantidade - this.defesa
+    if(quantidade < this.defesa){
+    dano = 0
+    }
+
+    this.life = this.life - dano
 
      if(this.life <=0){
         this.life = "morreu"
     }
 }
 estaVivo(){
-return this.vida > 0 
+return this.life > 0 
 }
 
 mostraStatus(){
-console.log(this.name + " | Vida: " + this.vida)
+console.log(this.name + " | Vida: " + this.life)
 
 }
 
@@ -29,13 +35,13 @@ console.log(this.name + " | Vida: " + this.vida)
 
 
 }
-const druida = new Personagem("Obin", 100, 15)
-const guerreiro = new Personagem("Gaia", 60, 20)
-const mago = new Personagem("Feiticeira", 30, 35)
-const arqueiro = new Personagem("Akaith", 80, 25)
-const elfo = new Personagem("Drazio", 35, 15)
-const dragao = new Personagem("Shibuya", 200, 80)
-const mutante = new Personagem("tempestade", 70, 50)
+const druida = new Personagem("Obin", 100, 15, 8)
+const guerreiro = new Personagem("Gaia", 60, 20, 15)
+const mago = new Personagem("Feiticeira", 30, 35, 25)
+const arqueiro = new Personagem("Akaith", 80, 25, 12)
+const elfo = new Personagem("Drazio", 35, 15, 12)
+const dragao = new Personagem("Shibuya", 200, 80, 72)
+const mutante = new Personagem("tempestade", 70, 50, 46)
 
 
 
